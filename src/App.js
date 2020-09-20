2f7e7d1fb61f564f64f4517b95257e2a8d618290
mdsayeed6220 <sayeedofficial6220@gmail.com>
2020-09-20 14:21:10 +0530

    Made a Http Request from Github Api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React  from 'react';
-import Navbar from './Components/Layout/Navbar'
+import React from "react";
+import Navbar from "./Components/Layout/Navbar";
+import axios from "axios";
+import "./App.css";
+import Users from "./Users/Users";
+class App extends React.Component {
+  state = {
+    users: [],
+    loading: false,
+  };
 
-import './App.css';
-import Users from './Users/Users'
-function App() {
-  return (
-    <div className="App">
-      <Navbar  ></Navbar>
-      <Users></Users>
-    </div>
-  );
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get("https://api.github.com/users");
+    this.setState({ users: res.data, loading: false });
+    console.log(res.data);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar></Navbar>
+        <Users loading={this.state.loading} users={this.state.users}></Users>
+      </div>
+    );
+  }
 }
 
 export default App;
