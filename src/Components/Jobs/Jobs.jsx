511d82c01c9ec8796456c56ea9ee6c5377af1a47
mdsayeed6220 <sayeedofficial6220@gmail.com>
2020-10-01 18:19:47 +0530

    Removed FetchJobs for SomeReason

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import useFetchJobs from "./useFetchJobs";
+
 function Jobs() {
-    return <div>
-      <h2>Welcome to th jobs</h2>
-  </div>;
+  return (
+    <div>
+      <h1>Welcome to th jobs</h1>
+    </div>
+  );
 }
 
 export default Jobs;
