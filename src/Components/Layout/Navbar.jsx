8d3e5f56891b2042e810f688d8137d49e0ecb639
mdsayeed6220 <sayeedofficial6220@gmail.com>
2020-10-05 18:52:39 +0530

    minor changes

--- src/Components/Layout/Navbar.jsx
+++ src/Components/Layout/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = ({ icon, title }) => {
   );
 };
 Navbar.defaultProps = {
-  title: "Geek 💡 Hub",
+  title: "Geek's 💡 Hub",
   icon: "fab fa-github  fa-spin",
 };
 // should give string type
